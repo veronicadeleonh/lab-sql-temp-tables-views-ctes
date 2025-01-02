bca435517ee3072e9630824a91398e24d29725c2
veronicadeleonh <v.dleon@gmail.com>
2025-01-02 14:49:59 +0100

    small fixes

--- lab-sql-temp-tables-views-ctes.sql
+++ lab-sql-temp-tables-views-ctes.sql
@@ -8,14 +8,14 @@ SELECT * FROM rental;
 CREATE VIEW rental_summary AS
 SELECT 
 	c.customer_id, 
-    c.first_name, 
-    c.last_name, 
+	CONCAT(c.first_name, ' ', c.last_name) AS customer_name,
     c.email, 
     COUNT(r.rental_id) AS rental_count
 FROM customer AS c
 JOIN rental AS r
 ON c.customer_id = r.customer_id
-GROUP BY customer_id;
+GROUP BY customer_id
+ORDER BY rental_count DESC;
 
 SELECT * FROM rental_summary;
 
@@ -40,6 +40,7 @@ ORDER BY total_paid DESC;
 
 # 3 
 
+WITH customer_summary_report AS(
 SELECT 
 	CONCAT(cp.first_name, ' ', cp.last_name) AS name,
     cp.email,
@@ -49,7 +50,8 @@ SELECT
 FROM temp_customer_payments AS cp
 JOIN rental_summary AS rs
 ON cp.customer_id = rs.customer_id
-ORDER BY avg_payment_per_rental DESC;
+ORDER BY avg_payment_per_rental DESC
+);
 
 
 
